ea300a539ade08febf7898ca06230bafd51a6055
John Smith <johnsmithcoder1@gmail.com>
2023-01-02 10:52:44 -0600

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by J S on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
