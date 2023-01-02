5cddc0720de427b40a7fca6fbf58f71b92b005ed
John Smith <johnsmithcoder1@gmail.com>
2023-01-02 11:34:27 -0600

    Add stepper to control animation amount

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -11,24 +11,34 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Tap Me") {
+        print(animationAmount)
+        
+        return VStack {
+            Spacer()
             
+            HStack {
+                Spacer()
+                Stepper("Scale amount", value: $animationAmount.animation(
+                    .easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+                ), in: 1...10)
+                Spacer()
+            }
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            
+            Spacer()
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(.easeInOut(duration: 1)
-                    .repeatForever(autoreverses: false), value: animationAmount)
-        )
-        .onAppear {
-            animationAmount = 2
-        }
+       
     }
 }
 
